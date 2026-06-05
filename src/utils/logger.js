7d5c237e28e5845b6ebf1a4d9f641dd39aa5b0ff
ismail2737351 <asmaeelsafe0@gmail.com>
2026-06-05 23:15:38 +0300

    Add logger utility

---
+++ src/utils/logger.js
@@ -0,0 +1,30 @@
+const winston = require('winston');
+const config = require('../../config');
+
+const logger = winston.createLogger({
+  level: config.logging.level,
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.errors({ stack: true }),
+    winston.format.printf((info) => {
+      const { timestamp, level, message, stack } = info;
+      const log = `${timestamp} [${level.toUpperCase()}] ${message}`;
+      return stack ? `${log}\n${stack}` : log;
+    })
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.printf((info) => {
+          const { timestamp, level, message } = info;
+          return `${timestamp} [${level}] ${message}`;
+        })
+      ),
+    }),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+});
+
+module.exports = logger;
